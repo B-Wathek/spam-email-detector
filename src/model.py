6c9da61586ccfab1fb33e9f35871d30041307140
B-Wathek <153394978+B-Wathek@users.noreply.github.com>
2026-05-02 18:04:59 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,7 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
+def build_model():
+    vectorizer = TfidfVectorizer(stop_words='english')
+    model = LogisticRegression()
+    return vectorizer, model
