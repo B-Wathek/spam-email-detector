841b2569b3c7edeadf00edf9eb189668d0324701
B-Wathek <153394978+B-Wathek@users.noreply.github.com>
2026-05-02 19:44:06 +0300

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,18 @@
+import pandas as pd
 from src.data import load_data
 
-def test_data_loading():
-    df = load_data("data/spam.csv")
-    assert not df.empty
+def test_data_loading(tmp_path):
+    # Create fake dataset
+    file = tmp_path / "test.csv"
+
+    df = pd.DataFrame({
+        "text": ["hello", "buy now"],
+        "label": [0, 1]
+    })
+
+    df.to_csv(file, index=False)
+
+    # Load using your function
+    df_loaded = load_data(file)
+
+    assert not df_loaded.empty
